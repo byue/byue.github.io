92610c0be3e0f41e1631d426ae84fe83498a8bbd
WhenPterodactylsAttack <62811100+WhenPterodactylsAttack@users.noreply.github.com>
2024-09-01 15:27:52 -0700

    added color themes (#13)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "rgb(56 189 248)",
+        black: "rgb(0 0 0)",
+        white: "rgb(255 255 255)"
+      }
+    },
   },
   plugins: [],
 }
