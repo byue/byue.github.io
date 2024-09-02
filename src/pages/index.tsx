a656f64fb5a8cc88a2336a4bfa08f772225f7786
Dylan Yue <mrmeepers322@gmail.com>
2024-09-01 22:11:43 -0700

    Changed tab id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -103,4 +103,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Architect Solutions</title>
