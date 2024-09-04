05dc437ee21d1c10f3f187b2dd3bde2dde2b9532
Dylan Yue <mrmeepers322@gmail.com>
2024-09-04 00:29:01 -0700

    Added button functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,13 +45,13 @@ const data = [
   {
     title: "Services",
     content: (
-      <FocusCards />
+        <FocusCards />
     ),
   },
   {
     title: "Skills",
     content: (
-      <div className="pl-[5rem] h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
+      <div id="Skills" className="pl-[5rem] h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40">
         <Tabs />
       </div>
     ),
@@ -59,19 +59,46 @@ const data = [
   {
     title: "Customers",
     content: (
-      <h1>Who we work with</h1>
+      <h1 id="Customers">Who we work with</h1>
     ),
   },
   {
     title: "Contact",
     content: (
       <div>
-        <h1> Email: bryan.yue@architech-solutions.com</h1>
+        <h1 id="Contact"> Email: bryan.yue@architech-solutions.com</h1>
         <h1> Phone: xxx-xxx-xxxx</h1>
       </div>
     ),
   },
 ];
+
+const scrollWithOffset = ( buttonLabel: string) => {
+  var offset;
+  const element = document.getElementById(buttonLabel);
+  const rect = element.getBoundingClientRect();
+  const elementTop = rect.top + window.scrollY;
+  if(buttonLabel === "About Us") {
+    offset = 200;
+  } else if(buttonLabel === "Skills") {
+    offset = 25
+  } else if(buttonLabel === "Services") {
+    offset = 10
+  } else {
+    offset = 0;
+  }
+  window.scrollTo({ top: elementTop - offset, behavior: 'smooth' });
+}
+
+const handleClick = (buttonLabel) => {
+  const element = document.getElementById(buttonLabel);
+  if(buttonLabel === "Home") {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  } else {
+    scrollWithOffset(buttonLabel);
+  }
+}
+
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles} className="bg-black">
@@ -79,14 +106,17 @@ const IndexPage: React.FC<PageProps> = () => {
         <ParticleLinks className="absolute w-full h-full"/>
         <div id="nav_bar" className="flex flex-wrap justify-center space-x-4 mb-24">
           {['Home', 'About Us', 'Services', 'Skills', 'Customers', 'Contact'].map(buttonLabel => 
-            <button className="flex-auto text-white bg-black min-w-fit relative h-[50px] sm:w-[5rem] md:w-[7rem] lg:w-[12rem] overflow-hidden rounded-full border-2 border-[#616467] px-3 text-white uppercase
+            <button  
+              key={buttonLabel}
+              onClick={() => handleClick(buttonLabel)}
+              className="flex-auto text-white bg-black min-w-fit relative h-[50px] sm:w-[5rem] md:w-[7rem] lg:w-[12rem] overflow-hidden rounded-full border-2 border-[#616467] px-3 text-white uppercase
                                 font-light shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-10 before:h-full before:w-0 before:bg-sky-500 before:transition-all 
                                 before:duration-300 hover:text-white hover:shadow-sky-500 hover:before:left-0 hover:before:w-full">
               <span className="relative z-10 sm:xs md:text-sm lg:text-2xl xl:text-2xl">{buttonLabel}</span>
             </button>
           )}
         </div>
-        <h1 id="main_title" className="text-3xl sm:text-5xl md:text-7xl lg:text-9xl font-bold text-center text-white relative mx-auto w-full z-20">
+        <h1 id="Main Page" className="text-3xl sm:text-5xl md:text-7xl lg:text-9xl font-bold text-center text-white relative mx-auto w-full z-20">
           Architech Solutions
         </h1>
         <div className="w-full sm:w-[24rem] md:w-[48rem] lg:w-[72rem] h-1 relative items-center justify-center">
@@ -103,7 +133,7 @@ const IndexPage: React.FC<PageProps> = () => {
         {/* Radial gradient for the container to give a faded look */}
         <div className="z-0 absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div id="about us" className="z-10 bg-transparent column px-[11rem]">
-          <h1 className=" text-white text-[4rem] text-center font-medium mt-[22rem] mb-[2rem]">
+          <h1 id="About Us" className=" text-white text-[4rem] text-center font-medium mt-[22rem] mb-[2rem]">
             About Us
           </h1>
           <div className="text-4xl pl-[2rem] mx-auto text-white text-left tracking-wider mb-[22rem] font-normal">
