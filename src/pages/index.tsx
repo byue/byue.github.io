e01b169aa29ea51786b707744429816df676085b
WhenPterodactylsAttack <62811100+WhenPterodactylsAttack@users.noreply.github.com>
2024-09-06 20:49:52 -0700

    Added contact us elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ import { FlipWords } from "../components/ui/flip-words";
 import { FocusCards } from "../components/ui/focus-cards";
 import { Tabs } from "../components/ui/tabs";
 import scrollTo from 'gatsby-plugin-smoothscroll';
-
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { cn } from "../lib/utils";
 
 const pageStyles = {
   color: "#232129",
@@ -43,6 +45,34 @@ const words = [
   },
 ];
 
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  console.log("Form submitted");
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full text-wrap", className)}>
+      {children}
+    </div>
+  );
+};
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
 const timelineData = [
   {
     title: "Services",
@@ -58,20 +88,52 @@ const timelineData = [
       </div>
     ),
   },
-  /*
-  {
-    title: "Customers",
-    content: (
-      <h1 id="customers">Who we work with</h1>
-    ),
-  },*/
   {
     title: "Contact",
     content: (
-      <div>
-        <h1 id="contact"> Email: bryan.yue@architech-solutions.com</h1>
-        <h1> Phone: xxx-xxx-xxxx</h1>
-      </div>
+<div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-[4rem] md:p-8 shadow-input bg-white dark:bg-black">
+      <h2 className="font-light text-3xl text-neutral-800 dark:text-neutral-200">
+        Reach Out to Us
+      </h2>
+      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300 font-light">
+        Have questions? Send us an email!
+      </p>
+      <form className="my-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label className="font-light" htmlFor="firstname">First name</Label>
+            <Input className="font-light" id="firstname" placeholder="First" type="text" />
+          </LabelInputContainer>
+          <LabelInputContainer>
+            <Label className="font-light" htmlFor="lastname">Last name</Label>
+            <Input className="font-light" id="lastname" placeholder="Last" type="text" />
+          </LabelInputContainer>
+        </div>
+        <div className="relative">
+        <LabelInputContainer className="mb-4">
+          <Label className="font-light" htmlFor="email">Email Address</Label>
+          <Input className="font-light" id="email" placeholder="Your email" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4 text-wrap h-40 font-light">
+          <Label className="font-light" htmlFor="issue">Your Problem</Label>
+          <textarea placeholder="Tell us about your problem! We'll be glad to help out." className="flex-none h-[9rem] w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 text-sm  file:border-0 file:bg-transparent 
+          file:text-sm file:font-light placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
+          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+           disabled:cursor-not-allowed disabled:opacity-50
+           dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+           group-hover/input:shadow-none transition duration-400 overflow-x-hidden inline-block align-top text-left align-top leading-normal resize-none"></textarea>
+        </LabelInputContainer>
+        <button
+          className="font-light absolute pt-[5-rem] bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          Send Email
+          <BottomGradient />
+        </button>
+        </div>
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+      </form>
+    </div>
     ),
   },
 ];
