11d6acf0ec98b25f6008680f76558c6a18e4c55a
WhenPterodactylsAttack <62811100+WhenPterodactylsAttack@users.noreply.github.com>
2024-09-07 01:41:26 -0700

    Added email functionality (#37)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,10 +45,6 @@ const words = [
   },
 ];
 
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  console.log("Form submitted");
-};
 
 const LabelInputContainer = ({
   children,
@@ -91,49 +87,54 @@ const timelineData = [
   {
     title: "Contact",
     content: (
-<div id="contact" className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-[4rem] md:p-8 shadow-input bg-white dark:bg-black">
-      <h2 className="font-light text-3xl text-neutral-800 dark:text-neutral-200">
-        Reach Out to Us
-      </h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300 font-light">
-        Have questions? Send us an email!
-      </p>
-      <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-          <LabelInputContainer>
-            <Label className="font-light" htmlFor="firstname">First name</Label>
-            <Input className="font-light" id="firstname" placeholder="First" type="text" />
+      <div id="contact" className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-[4rem] md:p-8 shadow-input bg-black">
+        <h2 className="font-light text-3xl text-neutral-200">
+          Reach Out to Us
+        </h2>
+        <p className=" text-sm max-w-sm mt-2 text-neutral-300 font-light">
+          Have questions? Send us an email here!
+        </p>
+        <form className="my-8" method="POST" action={process.env.GATSBY_WEB3_API_URL}>
+          <input type="hidden" name="access_key" value={process.env.GATSBY_WEB3_API_KEY}></input>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <Label className="font-light" htmlFor="First-Name">First Name</Label>
+              <Input className="font-light" id="name" placeholder="First Name" type="text" name="First-Name" />
+            </LabelInputContainer>
+            <LabelInputContainer>
+              <Label className="font-light" htmlFor="Last-Name">Last Name</Label>
+              <Input className="font-light" id="name" placeholder="Last Name" type="text" name="Last-Name" />
+            </LabelInputContainer>
+          </div>
+          <div className="relative">
+          <LabelInputContainer className="mb-4">
+            <Label className="font-light" htmlFor="Email">Email</Label>
+            <Input className="font-light" id="email" placeholder="E.g: myemail@gmail.com" type="email" name="Email" />
           </LabelInputContainer>
-          <LabelInputContainer>
-            <Label className="font-light" htmlFor="lastname">Last name</Label>
-            <Input className="font-light" id="lastname" placeholder="Last" type="text" />
+          <LabelInputContainer className="mb-4">
+            <Label className="font-light" htmlFor="Subject">Subject</Label>
+            <Input className="font-light" id="email" placeholder="Enter your email subject here." type="subject" name="Subject" />
           </LabelInputContainer>
-        </div>
-        <div className="relative">
-        <LabelInputContainer className="mb-4">
-          <Label className="font-light" htmlFor="email">Email Address</Label>
-          <Input className="font-light" id="email" placeholder="Your email" type="email" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4 text-wrap h-40 font-light">
-          <Label className="font-light" htmlFor="issue">Your Problem</Label>
-          <textarea placeholder="Tell us about your problem! We'll be glad to help out." className="flex-none h-[9rem] w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 text-sm  file:border-0 file:bg-transparent 
-          file:text-sm file:font-light placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
-           disabled:cursor-not-allowed disabled:opacity-50
-           dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
-           group-hover/input:shadow-none transition duration-400 overflow-x-hidden inline-block align-top text-left align-top leading-normal resize-none"></textarea>
-        </LabelInputContainer>
-        <button
-          className="font-light absolute pt-[5-rem] bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
-        >
-          Send Email
-          <BottomGradient />
-        </button>
-        </div>
-        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-      </form>
-    </div>
+          <LabelInputContainer className="mb-4 text-wrap h-40 font-light">
+            <Label className="font-light" htmlFor="issue">Message</Label>
+            <textarea name="Message" placeholder="Tell us about your problem! We'll be glad to help out." className="flex-none m-[2px] h-[9rem] w-full border-none bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 text-sm  file:border-0 file:bg-transparent 
+            file:text-sm file:font-light placeholder:placeholder-text-neutral-600 
+            focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-600
+            disabled:cursor-not-allowed disabled:opacity-50
+            dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+            group-hover/input:shadow-none transition duration-400 overflow-x-hidden inline-block align-top text-left align-top leading-normal resize-none"></textarea>
+          </LabelInputContainer> 
+          <button
+            className="font-light absolute pt-[5-rem] bg-gradient-to-br relative group/btn from-black from-zinc-900 to-zinc-900 to-neutral-600 block bg-zinc-800 w-full text-white rounded-md h-10 shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+          >
+            Send Email
+            <BottomGradient />
+          </button>
+          </div>
+          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+        </form>
+      </div>
     ),
   },
 ];
