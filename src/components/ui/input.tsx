0524da0f3222a3ea0af07645a387f8755f7ddcfb
Dylan Yue <mrmeepers322@gmail.com>
2024-09-07 02:13:58 -0700

    Fixed text. Added security features.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -37,15 +37,15 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         onMouseLeave={() => setVisible(false)}
         className="p-[2px] rounded-lg transition duration-300 group/input"
       >
-        <input
+        <textarea
           type={type}
           className={cn(
             `flex-none h-10 w-full border-none bg-zinc-800 text-white shadow-input rounded-md px-3 text-sm  file:border-0 file:bg-transparent 
-          file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+          file:text-sm file:font-medium placeholder:text-neutral-400
+          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400
            disabled:cursor-not-allowed disabled:opacity-50
-           dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
-           group-hover/input:shadow-none transition duration-400 overflow-x-hidden inline-block
+           shadow-[0px_0px_1px_1px_var(--neutral-700)]
+           group-hover/input:shadow-none transition duration-400 overflow-x-hidden inline-block resize-none
            `,
             className
           )}
