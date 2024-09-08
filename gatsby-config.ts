8e01909b552fb7e6f9dba9c7827c794710ef363b
bryue <brandonyue@hotmail.com>
2024-09-07 20:55:22 -0400

    fix incorrect site url in gatsby-config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,7 +7,7 @@ require("dotenv").config({
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Architech Solutions`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.architech-solutions.com/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
