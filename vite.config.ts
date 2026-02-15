ae2d874663019921325d7815e9765dd827440868
Bryan Yue <yue_bryan123@hotmail.com>
2026-02-15 09:44:24 -0800

    Fix base

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repositoryName =
-  process.env.VITE_REPO_NAME ?? process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "byue.github.io";
-const explicitBase = process.env.VITE_BASE_PATH;
-const isUserSiteRepo = repositoryName.endsWith(".github.io");
-
-const base =
-  explicitBase ?? (process.env.NODE_ENV === "production" && !isUserSiteRepo ? `/${repositoryName}/` : "/");
-
 export default defineConfig({
   plugins: [react()],
-  base,
 });
